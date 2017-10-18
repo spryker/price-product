05f61bd00de10dcadb08afe5ccbd90e2b2c6bcba
Auris <aurimas.lickus@spryker.com>
2017-10-18 17:18:06 +0200

    core-1750: product multicurrency

---
+++ src/Spryker/Client/PriceProduct/Dependency/Client/PriceProductToPriceInterface.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright © 2017-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Client\PriceProduct\Dependency\Client;
+
+interface PriceProductToPriceInterface
+{
+    /**
+     * @return string
+     */
+    public function getCurrentPriceMode();
+
+    /**
+     * @return string
+     */
+    public function getGrossPriceModeIdentifier();
+
+    /**
+     * @return string
+     */
+    public function getNetPriceModeIdentifier();
+}
