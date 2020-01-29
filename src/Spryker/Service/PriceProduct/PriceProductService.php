3843856fb0f71008cc0a2c61fdce7eb39be5880e
vseselov <alexander.veselov@spryker.com>
2020-01-29 15:48:37 +0200

    MP-1937: created service resolveProductPriceByPriceProductFilter method

---
+++ src/Spryker/Service/PriceProduct/PriceProductService.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Service\PriceProduct;
+
+use Generated\Shared\Transfer\PriceProductCriteriaTransfer;
+use Generated\Shared\Transfer\PriceProductFilterTransfer;
+use Generated\Shared\Transfer\PriceProductTransfer;
+use Spryker\Service\Kernel\AbstractService;
+
+/**
+ * @method \Spryker\Service\PriceProduct\PriceProductServiceFactory getFactory()
+ */
+class PriceProductService extends AbstractService implements PriceProductServiceInterface
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\PriceProductTransfer[] $priceProductTransfers
+     * @param \Generated\Shared\Transfer\PriceProductFilterTransfer $priceProductFilterTransfer
+     *
+     * @return \Generated\Shared\Transfer\PriceProductTransfer|null
+     */
+    public function resolveProductPriceByPriceProductFilter(array $priceProductTransfers, PriceProductFilterTransfer $priceProductFilterTransfer): ?PriceProductTransfer
+    {
+        return $this->getFactory()
+            ->createPriceProductMatcher()
+            ->matchPriceByFilter($priceProductTransfers, $priceProductFilterTransfer);
+    }
+}
